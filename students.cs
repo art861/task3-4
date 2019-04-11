d782542e62ce8d63a9f7ffda82063353561d14e9
art861 <49514164+art861@users.noreply.github.com>
2019-04-11 11:26:08 +0300

    Add files via upload

--- students.cs
+++ students.cs
@@ -1,23 +1,24 @@
 ﻿using System;
-namespace v0dot3
+namespace v0dot4
 {
     public class students
     {
 
         String fullName;
         String surName;
-        int HW1,HW2,HW3,HW4,HW5,Exam;
-        
-        public students(String fullName,String surName,int HW1, int HW2, int HW3, int HW4, int HW5, int Exam)
+        int HW1, HW2, HW3, HW4, HW5, Exam,FP;
+
+        public students(String fullName, String surName, int HW1, int HW2, int HW3, int HW4, int HW5, int Exam)
         {
             this.fullName = fullName;
             this.surName = surName;
             this.HW1 = HW1;
             this.HW2 = HW2;
             this.HW3 = HW3;
-            this.HW4 = HW4; 
+            this.HW4 = HW4;
             this.HW5 = HW5;
             this.Exam = Exam;
+
         }
 
         public String getName()
@@ -53,6 +54,27 @@ namespace v0dot3
         public int getExam()
         {
             return this.Exam;
+        }
+
+        public double getFP()
+        {
+            double studentMarks = this.HW1 + this.HW2 + this.HW3 + this.HW4 + this.HW5;
+            double finalPointAvg = ((0.3) * (studentMarks / 5)) + (0.7 * this.Exam);
+            return finalPointAvg;
+        }
+
+        public void print()
+        {
+          //  Console.WriteLine(this.HW1 + "--" + this.HW2 + "--" + this.HW3 + "--" + this.HW4 + "--" + this.HW5 + "--" + this.Exam);
+            Console.WriteLine(string.Format("{0,-21}   {1,-13}  {2,6}    ", this.fullName, this.surName, getFP()));
+
+        }
+
+        public string getData()
+        {
+            //  Console.WriteLine(this.HW1 + "--" + this.HW2 + "--" + this.HW3 + "--" + this.HW4 + "--" + this.HW5 + "--" + this.Exam);
+            return (this.fullName + " " + this.surName + " " + getFP());
+
         }
     }
 }
